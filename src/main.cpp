c91b059a2f013501977caef7c081beeb94bd8f8e
juraev <kh.j.mx@mail.ru>
2019-01-03 21:55:14 +0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jan 3, 2019
+ *      Author: jack
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hello World!";
+	return 0;
+}
+
+
