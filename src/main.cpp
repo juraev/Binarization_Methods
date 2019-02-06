085509068755307887e8679c101bb26b2453f5b7
khurshid <khurshid@hiro>
2019-02-06 12:38:58 +0500

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -7,27 +7,86 @@
 
 #include <iostream>
 #include "Image.h"
+#include <fstream>
+#include <sys/time.h>
 
 using namespace std;
 
-int main(){
-	string s = "image.jpg";
-	//cin >> s;
+long long getTime(){
+	struct timeval tp;
+	gettimeofday(&tp, NULL);
+	long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+	return ms;
+}
+
+int main(int argc, char** args){
+	ofstream file("/home/jack/eclipse-workspace/Binarization-Methods/stats.csv", std::ios::app);
+	string s, s1;
+
+	s = args[1];
+	s1 = args[2];
+	s += s1;
+
+	//	s = "image1.jpg";
+//	cin >> s;
 	Image<> img(s);
-	CImg<unsigned char>* bernsen = img.bernsenTech(25);
+	int b, sa, n, ot;
+	long long result, result1;
+
+//	result = getTime();
+//	CImg<unsigned char>* bernsen = img.bernsenTech(25);
+//	result1 = getTime();
+//	b = result1 - result;
+
+	result = getTime();
 	CImg<unsigned char>* sauvola = img.SauvolaTech();
-	CImg<unsigned char>* niblack = img.NiblackTech();
-	bernsen->save_jpeg("bernsen.jpg");
-	sauvola->save_jpeg("sauvola.jpg");
-	niblack->save_jpeg("niblack.jpg");
+	result1 = getTime();
+	sa = result1 - result;
 
-	CImgDisplay disp(*bernsen);
-	CImgDisplay disp1(*sauvola);
-	CImgDisplay disp2(*niblack);
-	while(!disp.is_closed()&&!disp1.is_closed()&&!disp2.is_closed()){
+//	result = getTime();
+//	CImg<unsigned char>* niblack = img.NiblackTech();
+//	result1 = getTime();
+//	n = result1 - result;
 
+	result = getTime();
+	CImg<unsigned char>* otsu = img.OtsuTech();
+	result1 = getTime();
+	ot = result1 - result;
+	string sss = "";
+	int i = 0;
+	while(true){
+		if(args[2][i] == '.') break;
+		sss += args[2][i];
+		i ++;
 	}
+	s = "/home/khurshid/Downloads/output/"+sss;
+	s1 = "mkdir -p " + s;
+	//system(s1.c_str());
+//
+//	s1 = s + "/bernsen.jpg";
+//	bernsen->save_jpeg(s1.c_str());
 
+	s1 = s + "/sauvola.jpg";
+	sauvola->save_jpeg("/home/khurshid/Downloads/sauvola.jpg");
+
+//	s1 = s + "/niblack.jpg";
+//	niblack->save_jpeg(s1.c_str());
+
+	s1 = s + "/otsu.jpg";
+	otsu->save_jpeg(s1.c_str());
+
+//	CImgDisplay disp(*bernsen, "Bernsen");
+//	CImgDisplay disp1(*sauvola, "Sauvola");
+//	CImgDisplay disp2(*niblack, "Niblack");
+//	CImgDisplay disp3(*otsu, "Otsu");
+
+	file << otsu->_width << "," << otsu->_height << "," << b << "," << n << "," << sa << "," << ot <<endl;
+	file.close();
+
+	//	while(!disp1.is_closed()){
+//	while(!disp.is_closed()&&!disp1.is_closed()&&!disp2.is_closed()&&!disp3.is_closed()){
+//
+//	}
 	return 0;
 }
 
